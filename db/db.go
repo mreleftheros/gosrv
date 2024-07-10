93031123b5067ecd7ab358dd3ace285942601f97
mreleftheros <eleftheros_all@protonmail.com>
2024-07-10 21:58:11 +0300

    t

---
+++ db/db.go
@@ -0,0 +1,7 @@
+package db
+
+import "fmt"
+
+func T(age int) {
+	fmt.Printf("You are %d", age)
+}
---
+++ server.go
@@ -0,0 +1,7 @@
+package gosrv
+
+import "fmt"
+
+func Hello(name string) {
+	fmt.Printf("Hello %s", name)
+}
